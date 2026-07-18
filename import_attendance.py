784198f100357c73c5fdf2adfe821ae480e9a50d
Abnowel <abimaame77@gmail.com>
2026-07-18 20:14:40 +0000

    Imported functions from attendance

--- import_attendance.py
+++ import_attendance.py
@@ -1,1 +1,14 @@
-import attendance
+from attendance import (initialize_attendance, mark_attendance, display_report,save_report)
+
+students = ["Rosebud","Abnowel","George","Jonathan","Oye"]
+
+attendance = initialize_attendance(students)
+
+total_days = 2
+
+for day in range(total_days):
+    print(f"\nDay{day + 1}")
+    mark_attendance(students, attendance)
+
+display_report(students, attendance, total_days)
+
