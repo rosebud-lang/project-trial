4be8cd547560005afbbf7e8c78faa1e62142c694
rosebud-lang <rhosebhurdawuah@gmail.com>
2026-07-18 16:56:42 +0100

    Add mark_attendance function

--- attendance.py
+++ attendance.py
@@ -9,3 +9,17 @@ def initialize_attendance(students):
     return attendance
 
 
+def mark_attendance(students, attendance):
+    """
+    Records attendance for one day.
+    """
+    print("\nMark Today's Attendance")
+    print("-" * 30)
+
+    for student in students:
+        status = input(f"Is {student} Present or Absent? ").strip().lower()
+
+        if status == "present":
+            attendance[student] += 1
+
+
