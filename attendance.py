31e2a3ae3962b8b6a4200764c297c2a1efede790
rosebud-lang <rhosebhurdawuah@gmail.com>
2026-07-18 15:56:44 +0100

    add initialize_attendance function

--- attendance.py
+++ attendance.py
@@ -1,1 +1,11 @@
-Attendance functions
+#Attendance functions
+def initialize_attendance(students):
+    """Creates a dictionary with each student starting at 0 attendance days."""
+    attendance = {}
+
+    for student in students:
+        attendance[student] = 0
+
+    return attendance
+
+
